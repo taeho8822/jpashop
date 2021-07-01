7679cafa819f85118ceb4f5dcc3322953a0bc29b
msline <zahir7@naver.com>
2021-07-01 20:58:32 +0900

    추가 수업

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,5 +22,6 @@ public class Member {
     private Address address;
 
     @OneToMany(mappedBy = "member")
+    @JsonIgnore
     private List<Order> orders = new ArrayList<>();
 }
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -85,4 +85,17 @@ public class OrderRepository {
 
         return query.getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class
+        )
+                .setFirstResult(1)
+                .setMaxResults(100)
+                .getResultList();
+    }
 }
